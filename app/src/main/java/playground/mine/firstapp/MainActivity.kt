1e53deb020db09c3ceb53b85ae4775926dcc9cc3
acidburn <cc.acidburn@gmail.com>
2023-05-12 23:42:20 +0200

    simple counter

--- app/src/main/java/playground/mine/firstapp/MainActivity.kt
+++ app/src/main/java/playground/mine/firstapp/MainActivity.kt
@@ -2,10 +2,24 @@ package playground.mine.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textChange = findViewById<TextView>(R.id.guza)
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        var counter = 0
+        btnClickMe.text = "Haha"
+        btnClickMe.setOnClickListener {
+            btnClickMe.text = "Hoho"
+
+            counter += 1
+
+            textChange.text = counter.toString()
+        }
     }
 }
